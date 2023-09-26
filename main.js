75ea71d621dab38f108c6250f04f0437a288a5cb
nick <108027865+nickaldwin@users.noreply.github.com>
2023-09-26 09:19:55 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 // answers to the js task
 
+//Variable task
+/*Working with variables
+importance: 2
+
+   1. Declare two variables: admin and name.
+   2. Assign the value "John" to name.
+   3. Copy the value from name to admin.
+   4. Show the value of admin using alert (must output “John”).
+
+   */
+
 
 
